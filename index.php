11cb51272a55623c53a2699719d1f5a3cff6d312
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-18 21:17:26 +0100

    change style

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (isset($_POST['submit'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/style.css">
 
   <!-- favicon -->
   <link rel="shortcut icon" href="res/favicon.png" type="image/x-icon">
